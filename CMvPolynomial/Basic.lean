f59837b75b68efd9dca59a63c2eeec94e87e8a6a
Andrei Burdusa <andrei.burdusa@zoho.com>
2025-05-05 14:39:39 +0300

    Define fromCMvPolynomial and toCMvPolynomial for variable indices of type Fin

--- CMvPolynomial/Basic.lean
+++ CMvPolynomial/Basic.lean
@@ -1,8 +1,16 @@
 import Batteries.Data.RBMap
 
 import Mathlib.Algebra.Ring.Nat
+import Mathlib.Order.Defs.LinearOrder
+import Mathlib.Data.List.Lex
+
+import Aesop
+
+open Batteries
 
 def hello := "world"
+
+#check List.le
 
 /-- Monomial in `n` variables. `#v[e₀, e₁, e₂]` denotes X₀^e₀ * X₁^e₁ * X₂^e₂ -/
 abbrev CMvMonomial n := Vector ℕ n
@@ -38,20 +46,183 @@ def orderingVector (m : CMvMonomial n) : GrevlexOrderingVector n :=
 -- #eval orderingVector #m[]
 -- #eval orderingVector #m[1, 2, 3]
 
-def grevlex (m1 m2 : CMvMonomial n) : Ordering :=
-  compare (totalDegree m1) (totalDegree m2) |>.then
-    (compareOfLessAndEq m2 m1)
+def grevlex (m₁ m₂ : CMvMonomial n) : Ordering :=
+  compare (totalDegree m₁) (totalDegree m₂) |>.then
+    (compareOfLessAndEq m₂ m₁)
+
+def simpleCmp [LT α] [DecidableLT α] [BEq α] (a₁ a₂ : α) : Ordering :=
+  if a₁ == a₂ then .eq else
+    if a₁ < a₂ then .lt else
+      .gt
+
+instance : Std.Irrefl fun (x1 x2 : ℕ) => x1 < x2 := by
+  constructor
+  apply Nat.lt_irrefl
+
+instance : Std.Total fun (x1 x2 : ℕ) => ¬x1 < x2 := by
+  constructor
+  intros a b
+  rw [Nat.not_lt, Nat.not_lt]
+  apply Nat.le_or_le b a
+
+instance : Std.Asymm fun (x1 x2 : ℕ) => x1 < x2 := by
+  constructor
+  intros a b h_lt
+  simp only [not_lt]
+  rw [Nat.le_iff_lt_or_eq]
+  simp [h_lt]
+
+instance :
+  Trans
+    (fun (x1 x2 : ℕ) => ¬x1 < x2)
+    (fun x1 x2 => ¬x1 < x2)
+    fun (x1 x2 : ℕ) => ¬x1 < x2
+:= by
+  constructor
+  intros a b c h₁ h₂
+  simp only [not_lt] at *
+  apply Nat.le_trans (m := b) <;> simp [*]
+
+def monomial_symm : ∀ (x y : CMvMonomial n),
+  (simpleCmp x y).swap = simpleCmp y x
+:= by
+  intros x y
+  unfold simpleCmp
+  split
+  case isTrue h =>
+    simp
+    simp only [beq_iff_eq] at h
+    subst h
+    simp only [↓reduceIte]
+  case isFalse h =>
+    simp [neq_of_not_iff] at h
+    simp_all only [beq_iff_eq]
+    have h' : y ≠ x := by
+      intro contra
+      subst contra
+      contradiction
+    simp [h']
+    split
+    case isTrue h_lt =>
+      have h' : ¬ y < x := by
+        apply Vector.lt_asymm (i := _)
+        · exact h_lt
+        · constructor; intros; simp [Nat.lt_asymm, *]
+      simp [Ordering.swap_lt]
+      split <;> trivial
+    case isFalse h_lt =>
+      simp_all only [ne_eq, Vector.not_lt, Ordering.swap_gt]
+      split
+      next h_1 => simp_all only
+      next h_1 =>
+        simp_all only [Vector.not_lt, reduceCtorEq]
+        rw [Vector.le_iff_lt_or_eq (xs := x)] at h_1
+        rw [Vector.le_iff_lt_or_eq (xs := y)] at h_lt
+        cases h_lt <;> cases h_1 <;> try contradiction
+        · apply Vector.lt_irrefl x
+          apply Vector.lt_trans (ys := y) <;> simp [*]
+
+lemma monomial_eq : ∀ m₁ m₂ : CMvMonomial n,
+  simpleCmp m₁ m₂ = .eq → m₁ = m₂
+:= by
+  unfold simpleCmp
+  intros m₁ m₂ h
+  split at h
+  case isTrue h_eq => simp_all only [beq_iff_eq]
+  case isFalse h_neq =>
+    simp_all only [beq_iff_eq]
+    split at h
+    next h_1 => simp_all only [reduceCtorEq]
+    next h_1 => simp_all only [Vector.not_lt, reduceCtorEq]
+
+
+lemma monomial_not_gt : ∀ m₁ m₂ : CMvMonomial n,
+  simpleCmp m₁ m₂ ≠ .gt ↔ m₁ ≤ m₂
+:= by
+  unfold simpleCmp; simp only [beq_iff_eq]
+  intro m₁ m₂
+  constructor
+  · intro h_cmp
+    split at h_cmp
+    case isTrue h_eq => subst h_eq; apply Vector.le_refl
+    case isFalse h_neq =>
+      split at h_cmp
+      case isTrue h_lt => simp only [Vector.le_iff_lt_or_eq, h_lt, true_or]
+      case isFalse => contradiction
+  · intro h_le
+    split
+    case mpr.isTrue => simp
+    case mpr.isFalse h_neq =>
+      have h_lt : m₁ < m₂ := by
+        rw [Vector.le_iff_lt_or_eq] at h_le
+        cases h_le
+        · simp only [*]
+        · contradiction
+      simp [*]
+
+def monomial_le_trans : ∀ {x y z : CMvMonomial n},
+  simpleCmp x y ≠ Ordering.gt →
+  simpleCmp y z ≠ Ordering.gt →
+  simpleCmp x z ≠ Ordering.gt
+:= by
+  intros x y z h_lt₁ h_lt₂
+  have h_lt₁' := (monomial_not_gt x y).1 h_lt₁
+  have h_lt₂' := (monomial_not_gt y z).1 h_lt₂
+  have h_lt₃ : x ≤ z := by
+    apply Vector.le_trans (ys := y) <;> simp [*]
+  rw [monomial_not_gt x z]
+  apply Vector.le_trans (ys := y) <;> simp only [*]
+
+
+instance : TransCmp (λ x1 x2 : CMvMonomial n => simpleCmp x1 x2) where
+  symm := monomial_symm
+  le_trans := monomial_le_trans
+
+instance : TransCmp (λ x1 x2 : (CMvMonomial n × R) => simpleCmp x1.1 x2.1) where
+  symm := by
+    intros
+    apply monomial_symm
+  le_trans := by
+    intros x y z
+    apply monomial_le_trans
 
 /-- Polynomial in `n` variables with coefficients in `R`. -/
-abbrev CMvPolynomial n R [CommSemiring R] :=
-  Batteries.RBMap (CMvMonomial n) R compare
+abbrev UnlawfulCMvPolynomial n R [CommSemiring R] :=
+  Batteries.RBMap (CMvMonomial n) R simpleCmp
 
-instance [Repr R] [CommSemiring R] : Repr (CMvPolynomial n R) where
+def UnlawfulCMvPolynomial.isNoZeroCoef [CommSemiring R]
+  (p : UnlawfulCMvPolynomial n R) : Prop
+:=
+  ∀ m, p.find? m ≠ some 0
+
+def CMvPolynomial n R [CommSemiring R] :=
+  { p : UnlawfulCMvPolynomial n R // p.isNoZeroCoef}
+
+def CMvPolynomial.monomialsList [CommSemiring R] (p : CMvPolynomial n R) :=
+  p.val.toList.unzip.1
+
+def CMvPolynomial.find? [CommSemiring R]
+  (p : CMvPolynomial n R)
+  (m : CMvMonomial n) :
+  Option R
+:=
+  p.val.find? m
+
+def CMvPolynomial.findD [CommSemiring R]
+  (p : CMvPolynomial n R) (m : CMvMonomial n) (v₀ : R) : R
+:=
+  (p.find? m).getD v₀
+
+-- #check (compare : (CMvMonomial 1) → (CMvMonomial 1) → Ordering)
+-- #check Vector.compare
+
+instance [Repr R] [CommSemiring R] : Repr (UnlawfulCMvPolynomial n R) where
   reprPrec p _ :=
     let toFormat : Std.ToFormat (CMvMonomial n × R) :=
       ⟨λ (m, c) => repr c ++ " * " ++ repr m⟩
     @Std.Format.joinSep _ toFormat p.toList " + "
 
-def myPolynomial : CMvPolynomial 3 ℕ := [⟨#m[1, 2, 1], 5⟩, ⟨#m[3, 2, 0], 5⟩].toRBMap compare
+def myPolynomial : UnlawfulCMvPolynomial 3 ℕ :=
+  [⟨#m[1, 2, 1], 5⟩, ⟨#m[3, 2, 0], 5⟩].toRBMap simpleCmp
 
 -- #eval myPolynomial
--- CMvPolynomial/Theorems.lean
+++ CMvPolynomial/Theorems.lean
@@ -1,5 +1,8 @@
 import CMvPolynomial.Basic
 import Mathlib.Algebra.MvPolynomial.Basic
+import Batteries.Data.RBMap.Alter
+
+open Batteries
 
 def finRangeNoDup (top n : Nat) (le_top_n : top ≤ n) :
   Σ' (l : List (Fin n)), (∀ x, x ∈ l ↔ x.val < top) ∧ List.Nodup l := by
@@ -53,10 +56,360 @@ def fromCMvMonomial (m : CMvMonomial n) : (Fin n →₀ ℕ) :=
       constructor <;> simp [support0, in_l_iff_lt a, *]
   Finsupp.mk support toFun mem_support_fun
 
-def toMvPolynomial [CommRing R] (p : CMvPolynomial n R) :
+lemma size_zero : ∀ (a : Array α), a.size = 0 → a = #[] := by
+  intros a h
+  rcases a with ⟨l⟩
+  cases l
+  · rfl
+  · contradiction
+
+lemma Finsupp.toFun_eq :
+  ∀ (f₁ f₂ : Fin n →₀ ℕ), f₁ = f₂ → f₁.toFun = f₂.toFun
+:= by
+  intro f₁ f₂ h_eq
+  rcases f₁ with ⟨_, toFun₁, _⟩
+  rcases f₂ with ⟨_, toFun₂, _⟩
+  simp
+  cases h_eq
+  rfl
+
+lemma vector_get_eq :
+  ∀ (v₁ v₂ : Vector ℕ n), Vector.get v₁ = Vector.get v₂ → v₁ = v₂
+:= by
+  induction n with
+  | zero =>
+    intros v₁ v₂ h
+    cases v₁; cases v₂
+    simp [size_zero, *]
+  | succ n' ih =>
+    intros v₁ v₂ h
+    rw [funext_iff] at h
+    aesop
+
+-- TODO: Use `σ` instead of `Fin n`
+def toCMvMonomial {n} (m : Fin n →₀ ℕ) : (CMvMonomial n) := by
+  cases h : n with
+  | zero => exact #m[]
+  | succ n' =>
+    have n_gt_0 : 0 < n := by
+      simp only [h, add_pos_iff, Nat.lt_one_iff, or_true]
+    rw [←h]
+    let init : Vector ℕ n := default
+    exact
+      init.mapFinIdx
+        λ i _ le_n => if ⟨i, le_n⟩ ∈ m.support then m.toFun ⟨i, le_n⟩ else 0
+
+lemma if_zero_then_zero : ∀ n : ℕ, (if n = 0 then 0 else n) = n := by
+  simp only [ite_eq_right_iff, forall_eq]
+
+theorem monomial_id₁ :
+  ∀ (m : CMvMonomial n), toCMvMonomial (fromCMvMonomial m) = m
+:= by
+  intro m
+  cases n with
+  | zero =>
+    have is_zero : m = #m[] := by
+      simp only [Vector.eq_mk, Vector.toArray_eq_empty_iff]
+    simp [is_zero]
+  | succ n' =>
+    unfold toCMvMonomial
+    simp only
+      [Fin.zero_eta, Finsupp.mem_support_iff, ite_not, eq_mpr_eq_cast, cast_eq]
+    unfold fromCMvMonomial
+    simp
+    split
+    rename_i x l in_l_iff_lt nodup heq
+    simp_all only [Finsupp.coe_mk]
+    obtain ⟨fst, snd⟩ := x
+    obtain ⟨left, right⟩ := snd
+    simp_all only [Fin.is_lt, iff_true]
+    simp only [if_zero_then_zero]
+    rw [Vector.mapFinIdx_eq_iff]
+    intro i h
+    rfl
+
+theorem Vector.get_at :
+  ∀ (v : Vector ℕ n) (f : Fin n → ℕ) (ix : Fin n),
+    (v.mapFinIdx λ i _ le_n => f ⟨i, le_n⟩)[ix] = f ix
+:= by
+  intros v f ix
+  apply Vector.getElem_mapFinIdx ix.2
+
+theorem monomial_id₂ :
+  ∀ (m : Fin n →₀ ℕ), fromCMvMonomial (toCMvMonomial m) = m
+:= by
+  intro m
+  cases n with
+  | zero => ext a; cases a; contradiction
+  | succ n' =>
+    unfold fromCMvMonomial
+    unfold toCMvMonomial
+    split
+    simp only [Fin.zero_eta, Finsupp.mem_support_iff, eq_mpr_eq_cast, cast_eq]
+    rcases m with ⟨sₘ, fₘ, memₘ⟩
+    simp only [Finsupp.coe_mk, ite_not, Finsupp.mk.injEq]
+    rename_i x l in_l_iff_lt nodup heq
+    simp only [if_zero_then_zero]
+    constructor
+    · ext a
+      rw [memₘ]
+      rw [←@Vector.get_at (n'+1) default fₘ a]
+      constructor
+      · intro h
+        simp only [Finset.mem_filter, Finset.mem_mk, Multiset.mem_coe] at h
+        let ⟨h₁, h₂⟩ := h
+        apply h₂
+      · intro h
+        simp only [Finset.mem_filter, Finset.mem_mk, Multiset.mem_coe]
+        constructor
+        · apply (in_l_iff_lt a).2
+          simp only [Fin.is_lt]
+        · apply h
+    · ext a
+      apply Vector.get_at
+
+def monomial_equiv : (CMvMonomial n ≃ (Fin n →₀ ℕ)) where
+  toFun := fromCMvMonomial
+  invFun := toCMvMonomial
+  left_inv := by
+    unfold Function.LeftInverse
+    apply monomial_id₁
+  right_inv := by
+    unfold Function.RightInverse
+    apply monomial_id₂
+
+def fromCMvMonomialEmbedding : CMvMonomial n ↪ (Fin n →₀ ℕ) :=
+  monomial_equiv.toEmbedding
+
+theorem monomial_list_nodup (l : List (CMvMonomial n)) :
+  l.Pairwise (RBNode.cmpLT simpleCmp) → l.Pairwise (· ≠ ·)
+:= by
+  induction l with
+  | nil => simp
+  | cons head tail ih =>
+    intro h
+    rcases h with _ | ⟨h₁, h₂⟩
+    apply List.Pairwise.cons
+    · intro a a_in
+      replace h₁ := h₁ a a_in
+      have instanceTransMonomial :
+        TransCmp (simpleCmp : CMvMonomial n → CMvMonomial n → Ordering) := by
+          infer_instance
+      simp [RBNode.cmpLT] at h₁
+      specialize @ h₁ instanceTransMonomial
+      intro contra
+      rw [contra] at h₁
+      simp [simpleCmp] at h₁
+    · apply ih
+      apply h₂
+
+lemma findFst (a : α) (l : List (α × β)) (a_in : a ∈ l.unzip.1) :
+  ∃ (b : β), (a, b) ∈ l
+:= by
+  simp at a_in
+  rcases a_in with ⟨x, pair_in⟩
+  exists x
+
+lemma pair_list_pairwise_lt (l : List (CMvMonomial n × R)) :
+  l.Pairwise (RBNode.cmpLT (simpleCmp ·.1 ·.1)) →
+    l.unzip.1.Pairwise (RBNode.cmpLT simpleCmp)
+:= by
+  intro h
+  induction h with
+  | nil => simp only [List.unzip_nil, List.Pairwise.nil]
+  | @cons a l head tail ih =>
+    apply List.Pairwise.cons
+    · intros a' a'_in
+      let ⟨r, r_in⟩ := findFst a' l a'_in
+      replace head := head (a', r) r_in
+      simp [RBNode.cmpLT] at head
+      have instanceTransMonomial :
+        TransCmp (λ x1 x2 : (CMvMonomial n × R) => simpleCmp x1.1 x2.1) := by
+          infer_instance
+      simp [RBNode.cmpLT, *]
+    · apply ih
+
+lemma list_pairwise_lt_nodup (l : List (CMvMonomial n)) :
+  l.Pairwise (RBNode.cmpLT simpleCmp) → l.Nodup
+:= by
+  intro h
+  induction h with
+  | nil => simp
+  | @cons a l head tail ih =>
+    apply List.Pairwise.cons
+    · intros a' a'_in contra
+      rw [contra] at head
+      specialize head a' a'_in
+      simp [RBNode.cmpLT] at head
+      specialize head
+      simp [simpleCmp] at head
+    · apply ih
+
+theorem monomials_nodup [CommRing R] (p : CMvPolynomial n R) :
+  p.monomialsList.Nodup
+:= by
+  apply list_pairwise_lt_nodup
+  apply pair_list_pairwise_lt
+  apply RBMap.toList_sorted
+
+theorem find_no_zero [CommRing R]
+  : ∀ (p : CMvPolynomial n R) (m : CMvMonomial n), p.find? m ≠ some 0
+:= by
+  intro p m
+  simp only [ne_eq]
+  intro contra
+  unfold CMvPolynomial UnlawfulCMvPolynomial.isNoZeroCoef at p
+  rcases p with ⟨p', ne_zero⟩
+  specialize ne_zero m
+  contradiction
+
+theorem find_no_zero_key [CommRing R]
+  : ∀ (p : CMvPolynomial n R) (m : CMvMonomial n),
+    m ∈ p.monomialsList → p.findD m 0 ≠ 0
+:= by
+  intros p m h_in
+  unfold CMvPolynomial.findD Option.getD
+  split
+  case h_1 v₀ v h_find =>
+    intro contra
+    unfold CMvPolynomial UnlawfulCMvPolynomial.isNoZeroCoef at p
+    rcases p with ⟨p', ne_zero⟩
+    apply ne_zero m
+    rw [←contra]
+    simp [CMvPolynomial.find?] at h_find
+    exact h_find
+  case h_2 v h_find =>
+    unfold CMvPolynomial.monomialsList at h_in
+    have ⟨val, find_val⟩ : ∃ r, p.find? m = some r := by
+      let pre@⟨b, h_b⟩ := findFst m (RBMap.toList p.val) h_in
+      have eq_m : simpleCmp m m = .eq := by simp [simpleCmp]
+      exists b
+      apply RBMap.find?_some.2
+      exists m
+    rw [h_find] at find_val
+    contradiction
+
+theorem find_no_zero_key' [CommRing R]
+  : ∀ (p : CMvPolynomial n R) (m : CMvMonomial n),
+    p.findD m 0 ≠ 0 → m ∈ p.monomialsList
+:= by
+  unfold CMvPolynomial.findD CMvPolynomial.find? Option.getD
+  intro p m h
+  split at h
+  case h_1 v₀ v h_find =>
+    apply RBMap.find?_some.1 at h_find
+    rcases h_find with ⟨y, hy⟩
+    unfold CMvPolynomial.monomialsList
+    simp_all only
+      [simpleCmp, List.unzip_fst, List.mem_map, Prod.exists]
+    simp only [exists_and_right, exists_eq_right]
+    obtain ⟨left, right⟩ := hy
+    aesop
+  case h_2 v₀ v h_find =>
+    simp_all only [ne_eq, not_true_eq_false]
+
+def fromCMvPolynomial [CommRing R]
+  (p : CMvPolynomial n R) :
   MvPolynomial (Fin n) R
 :=
-  let support : Finset ((Fin n) →₀ ℕ) := sorry
-  let toFun : (Fin n →₀ ℕ) → R := sorry
-  let mem_support_fun : ∀ (a : Fin n →₀ ℕ), a ∈ support ↔ toFun a ≠ 0 := sorry
+  let monomials := p.monomialsList
+  let monomials_nodup : monomials.Nodup := by apply monomials_nodup p
+  let monomials' := Multiset.ofList monomials
+  let monomials'_nodup : monomials'.Nodup := by
+    unfold monomials'
+    simp [monomials_nodup]
+  let support0 : Finset (CMvMonomial n) :=
+    { val := monomials'
+      nodup := monomials'_nodup
+    }
+  let support : Finset (Fin n →₀ ℕ) :=
+    support0.map fromCMvMonomialEmbedding
+  let toFun (f : Fin n →₀ ℕ) : R :=
+    let m := toCMvMonomial f
+    p.findD m 0
+  let mem_support_fun : ∀ (a : Fin n →₀ ℕ), a ∈ support ↔ toFun a ≠ 0 := by
+    intro a
+    constructor
+    · intro a_in
+      simp [support] at a_in
+      simp_all only [Finset.mem_mk, Multiset.mem_coe, ne_eq]
+      simp_all only [support0, monomials', monomials, support, toFun]
+      obtain ⟨w, h⟩ := a_in
+      obtain ⟨left, right⟩ := h
+      subst right
+      simp [fromCMvMonomialEmbedding, monomial_id₁, monomial_equiv]
+      apply find_no_zero_key
+      exact left
+    · intro a_ne_0
+      simp [support]
+      simp [fromCMvMonomialEmbedding]
+      simp only [toFun, monomials, monomials'] at a_ne_0
+      exists (toCMvMonomial a)
+      constructor
+      · apply find_no_zero_key'
+        exact a_ne_0
+      · apply monomial_id₂
   Finsupp.mk support toFun mem_support_fun
+
+-- TODO: Use `σ` instead of `Fin n`
+noncomputable def toCMvPolynomial [CommRing R]
+  (p : MvPolynomial (Fin n) R) :
+  CMvPolynomial n R
+:=
+  let {support, toFun, mem_support_toFun} := p
+  let monomials := support.toList
+  let unlawful :=
+    monomials.foldr
+    (fun m p => p.insert (toCMvMonomial m) (toFun m) )
+    (mkRBMap (CMvMonomial n) R simpleCmp)
+  { val := unlawful
+  , property := by
+      unfold UnlawfulCMvPolynomial.isNoZeroCoef
+      intro m
+      let m' := fromCMvMonomial m
+      have h : m' ∈ support ∨ ¬ m' ∈ support := by
+        apply Classical.em (m' ∈ support)
+      cases h with
+      | inl h_in =>
+        unfold unlawful
+        induction monomials with
+        | nil =>
+          simp
+          rw [RBMap.find?_some]
+          intro ⟨_, _, _⟩; contradiction
+        | cons head tail ih =>
+          simp only [RBMap.mkRBSet_eq, List.foldr_cons, RBMap.find?_insert] at *
+          split
+          case inl.cons.isTrue h_cmp =>
+            apply monomial_eq at h_cmp
+            subst h_cmp
+            simp_all only [ne_eq, Option.some.injEq, m']
+            intro a
+            apply h_in
+            simp only [monomial_id₂, a]
+          case inl.cons.isFalse => apply ih
+      | inr h_out =>
+        unfold unlawful
+        have h_out' : m' ∉ monomials := by simp [monomials, h_out]
+        revert h_out'
+        induction monomials with
+        | nil =>
+          simp
+          rw [RBMap.find?_some]
+          intro ⟨_, _, _⟩; contradiction
+        | cons head tail ih =>
+          simp only [RBMap.mkRBSet_eq, List.foldr_cons, RBMap.find?_insert] at *
+          intro h_out
+          simp only [List.mem_cons, not_or] at h_out
+          rcases h_out with ⟨h_out₁, h_out₂⟩
+          split
+          case inr.cons.intro.isTrue h_cmp =>
+            apply monomial_eq at h_cmp
+            subst h_cmp
+            have head_eq : m' = head := by
+              simp only [monomial_id₂, m']
+            contradiction
+          case inr.cons.intro.isFalse =>
+            apply ih
+            exact h_out₂
+  }

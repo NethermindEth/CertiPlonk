7d8ed18288af4f9a2a8c5cd9a988a52fe3b8f56e
Andrei Burdusa <andrei.burdusa@zoho.com>
2025-05-16 17:45:42 +0300

    WIP

--- CMvPolynomial/Basic.lean
+++ CMvPolynomial/Basic.lean
@@ -5,6 +5,7 @@ import Batteries.Data.RBMap
 -- import Mathlib.Data.List.Lex
 -- import Mathlib.Data.Quot
 -- import Mathlib.Data.Multiset.Basic
+-- import Data.Ord.
 import Mathlib
 import Aesop
 
@@ -239,27 +240,14 @@ where
     funext x
     simp [*]
 
-noncomputable def CMvPolynomial.toList [CommSemiring R]
-  (p : CMvPolynomial n R) :
-  List (CMvMonomial n × R)
-:=
-  -- Quotient.out p |>.val.toList
-  Quotient.lift (·.val.toList) valid p
-where
-  valid := sorry
-
-#check Multiset
-#check Quotient.lift
-
-
 def P [CommSemiring R]
   (f : CMvMonomial n → R → Multiset (CMvMonomial n) → Multiset (CMvMonomial n)) :
   Prop
 :=
   f = λ m _ acc => m ::ₘ acc
 
-def R' [CommSemiring R] (a b : UnlawfulCMvPolynomial n R) : Prop :=
-  ∀ (x : CMvMonomial n), a.find? x = b.find? x
+def incl [CommSemiring R] (a b : UnlawfulCMvPolynomial n R) : Prop :=
+  ∀ (m : CMvMonomial n) c, a.find? m = some c → b.find? m = some c
 
 def P' [CommSemiring R]
   (f : CMvMonomial n → R → Finset (CMvMonomial n) → Finset (CMvMonomial n)) :
@@ -274,26 +262,168 @@ lemma RBMap.size_zero [CommSemiring R] :
   rcases a with ⟨n⟩
   cases n <;> trivial
 
+#check RBMap
+
+theorem UnlawfulCMvPolynomial.nonemptySome [CommSemiring R]
+  (p : UnlawfulCMvPolynomial n R)
+  (h_size : p.size > 0) :
+  ∃ m r, p.find? m = some r
+:= by
+  unfold UnlawfulCMvPolynomial at *
+  rcases p with ⟨p⟩
+  cases p with
+    | nil => contradiction
+    | node _ l v r =>
+      rcases v with ⟨m, r⟩
+      exists m, r
+      simp
+        [ RBMap.find?, RBMap.findEntry?, RBSet.findP?, RBNode.find?, simpleCmp
+        , compareOfLessAndEq, gt_iff_lt
+        ]
+      split <;> (try simp [Vector.lt_irrefl] at *)
+
+-- #check RBMap.ofL
+
+theorem UnlawfulCMvPolynomial.removeOne [CommSemiring R]
+  (p : UnlawfulCMvPolynomial n R)
+  (sizePred : ℕ)
+  (h_size : p.size = sizePred + 1) :
+  ∃ (p' : UnlawfulCMvPolynomial n R),
+    p'.size = sizePred ∧
+      ∀ (m : CMvMonomial n) c, p'.find? m = some c → p.find? m = some c
+:= by
+  unfold UnlawfulCMvPolynomial at *
+  let pList := p.toList
+  rcases p with ⟨p⟩
+  cases p with
+    | nil => contradiction
+    | node _ l v r =>
+      let p' : UnlawfulCMvPolynomial n R :=
+        RBMap.ofList (l.toList ++ r.toList) simpleCmp
+      exists p'
+      constructor
+      · simp [RBMap.size, RBSet.size] at h_size
+        unfold p'
+        sorry
+      · sorry
+
+      -- rcases v with ⟨m, r⟩
+      -- exists m, r
+      -- simp
+      --   [ RBMap.find?, RBMap.findEntry?, RBSet.findP?, RBNode.find?, simpleCmp
+      --   , compareOfLessAndEq, gt_iff_lt
+      --   ]
+      -- split <;> (try simp [Vector.lt_irrefl] at *)
+
+-- #check RBMap.inse
+
+lemma erase_pred_size
+  {size' : ℕ}
+  (m : RBMap α β comparison)
+  (h_size : m.size = size' + 1)
+  (a : α) :
+  m.contains a → (m.erase a).size = size'
+:= sorry
+
+lemma match_insert
+  {size' : ℕ}
+  (m : RBMap α β comparison)
+  (h_size : m.size = size' + 1) :
+  ∃ (a : α) (b : β) (m' : RBMap α β comparison),
+    m'.size = size' ∧ m'.insert a b = m ∧ m'.find? a = none
+:= sorry
+
+-- #check RBMap.fold
+
 theorem fake' [CommSemiring R]
   (f : CMvMonomial n → R → Finset (CMvMonomial n) → Finset (CMvMonomial n))
   (a b : UnlawfulCMvPolynomial n R)
   (s_a s_b : ℕ)
   (p₁ : s_a = a.size)
   (p₂ : s_b = b.size)
-  (h_ext : R' a b)
+  (h_ext : incl a b)
   (h_f : P' f) :
   -- (h_m : P' m)
-  x ∈ RBMap.foldr f ∅ ↑a → x ∈ RBMap.foldr f ∅ ↑b
+  ∀ x, x ∈ RBMap.foldr f ∅ ↑a → x ∈ RBMap.foldr f ∅ ↑b
 := by
-  unfold R' at h_ext
+  unfold incl at h_ext
   unfold P' at h_f
   induction' s_a with s_a' ih generalizing a b s_b
   · have a_empty : a = ∅ := RBMap.size_zero a p₁.symm
     subst a_empty
-    · intro x_in_a
-      simp [RBMap.foldr_eq_foldr_toList] at x_in_a
-  · let a' := a.erase sorry
+    intro x x_in_a
+    simp [RBMap.foldr_eq_foldr_toList] at x_in_a
+  · intro x x_in_foldr_a
+    let ⟨m₀, r₀, a', h_size, h_insert, h_contains⟩ := match_insert a p₁.symm
+    specialize ih a' b s_b h_size.symm p₂
+    apply ih
+    intro m c h_find_a'
+    specialize h_ext m c
+    subst h_insert
+    apply h_ext
+    · sorry
+    · sorry
+
+#check RBSet.find?
+#check RBMap.find?
+#check RBSet.find?_some
+#check RBMap.find?_some
+#check RBMap.toList
+
+#check RBSet.contains_iff
+
+#check RBMap.find?_some_mem_toList
+#check RBMap.foldr_eq_foldr_toList
+#check RBMap.find?_some_mem_toList
+#check RBNode
+
+theorem fake'' [CommSemiring R]
+  (f : CMvMonomial n → R → Finset (CMvMonomial n) → Finset (CMvMonomial n))
+  (a b : UnlawfulCMvPolynomial n R)
+  (la lb : List (CMvMonomial n × R))
+  (p₁ : a.val.toList = la)
+  (p₂ : b.val.toList = lb)
+  (h_ext : incl a b)
+  (h_f : P' f) :
+  -- (h_m : P' m)
+  x ∈ List.foldr (fun p r => f p.1 p.2 r) ∅ la →
+  x ∈ List.foldr (fun p r => f p.1 p.2 r) ∅ lb
+:= by
+  induction' la with h tail ih generalizing a
+  · intro h₀
+    aesop
+  · rcases h with ⟨m, r⟩
+    intro h₀
+    simp at h₀
+    unfold P' at h_f
+    nth_rw 1 [h_f] at h₀; simp at h₀
+    unfold incl at h_ext
     sorry
+
+-- theorem fake''' [CommSemiring R]
+--   (f : CMvMonomial n → R → Finset (CMvMonomial n) → Finset (CMvMonomial n))
+--   (a b : RBNode (CMvMonomial n × R))
+--   (la lb : List (CMvMonomial n × R))
+--   (p₁ : a.toList = la)
+--   (p₂ : b.toList = lb)
+--   (h_ext : incl a b)
+--   (h_f : P' f) :
+--   -- (h_m : P' m)
+--   x ∈ List.foldr (fun p r => f p.1 p.2 r) ∅ la →
+--   x ∈ List.foldr (fun p r => f p.1 p.2 r) ∅ lb
+-- := by
+--   induction' la with h tail ih generalizing a
+--   · intro h₀
+--     aesop
+--   · rcases h with ⟨m, r⟩
+--     intro h₀
+--     simp at h₀
+--     unfold P' at h_f
+--     nth_rw 1 [h_f] at h₀; simp at h₀
+--     unfold incl at h_ext
+--     sorry
+
+#check RBMap.find?_some_mem_toList
 
 def CMvPolynomial.monomials' [CommSemiring R]
   (p : CMvPolynomial n R) :
@@ -311,19 +441,56 @@ where
       (fun m _ acc => insert m acc :
         CMvMonomial n → R → Finset (CMvMonomial n) → Finset (CMvMonomial n)
       ) = f
-    have h' : ∀ (x : CMvMonomial n), b.find? x = a.find? x := by simp [h]
-    refine
-      ⟨ fake' (f := f) (a := a.1) (b := b.1) (s_a := a.1.size) (s_b := b.1.size)
-          (p₁ := rfl)
-          (p₂ := rfl)
-          h
-          eq.symm
-      , fake' (f := f) (a := b.1) (b := a.1) (s_a := b.1.size) (s_b := a.1.size)
-          (p₁ := rfl)
-          (p₂ := rfl)
-          h'
-          eq.symm
-      ⟩
+    simp [RBMap.foldr_eq_foldr_toList]
+    unfold LawfulCMvPolynomial.find? at h
+    -- have h_list : ∀ (x : CMvMonomial n), a.find? x = b.find? x
+    generalize h_la : (RBMap.toList a.val) = la
+    generalize h_lb : (RBMap.toList b.val) = lb
+    induction la
+    · constructor
+      · intro h₀
+        aesop
+      · intro h₀
+        subst eq
+        simp_all only [List.foldr_nil, Finset.not_mem_empty]
+
+        sorry
+    -- simp [RBMap.find?_some_mem_toList] at h
+    -- generalize h₁ : (a.1 : RBSet _ _) = set₁
+    -- generalize h₂ : (b.1 : RBSet _ _) = set₂
+    -- unfold UnlawfulCMvPolynomial RBMap at set₁ set₂
+    -- have h' : ∀ (x : CMvMonomial n), b.find? x = a.find? x := by simp [h]
+    -- have h_in : ∀ (x : CMvMonomial n × R), set₁.contains x → set₂.contains x := by
+    --   intro x x_in
+    --   rcases x with ⟨m, r⟩
+    --   apply map_find_set set₁ m r at x_in
+    --   subst h₁ h₂
+    --   specialize h m
+    --   rw [h] at x_in
+    --   have _ : @TransCmp (CMvMonomial n × R) (Ordering.byKey Prod.fst simpleCmp) := sorry
+    --   apply (RBSet.mem_iff_find? (x := x) (t := set₂)).2
+    --   apply (RBSet.mem_iff_find? (x := x) (t := set₁)).1 at x_in
+    --   let ⟨y, hy⟩ := x_in
+    --   exists y
+    --   rw [RBSet.find?_some] at *
+    --   -- aesop
+    --   simp [LawfulCMvPolynomial.find?, RBMap.find?_some] at h
+    --   let ⟨m, c⟩ := y
+    --   specialize h m
+
+    sorry
+    -- refine
+    --   ⟨ fake' (f := f) (a := a.1) (b := b.1) (s_a := a.1.size) (s_b := b.1.size)
+    --       (p₁ := rfl)
+    --       (p₂ := rfl)
+    --       sorry
+    --       eq.symm
+    --   , fake' (f := f) (a := b.1) (b := a.1) (s_a := b.1.size) (s_b := a.1.size)
+    --       (p₁ := rfl)
+    --       (p₂ := rfl)
+    --       sorry
+    --       eq.symm
+    --   ⟩
 
 def CMvPolynomial.findD [CommSemiring R]
   (p : CMvPolynomial n R) (m : CMvMonomial n) (v₀ : R) : R

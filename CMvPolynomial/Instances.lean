2e77a5f61acd6af3d1db51aa4befc93655b286f6
Andrei Burdusa <andrei.burdusa@zoho.com>
2025-06-10 13:47:32 +0300

    Cleanup

--- CMvPolynomial/Instances.lean
+++ CMvPolynomial/Instances.lean
@@ -1,12 +1,12 @@
 import Mathlib
 
-instance : Std.Irrefl fun (x1 x2 : ℕ) => x1 < x2 := ⟨Nat.lt_irrefl⟩
+instance : Std.Irrefl λ x1 x2 : ℕ => x1 < x2 := ⟨Nat.lt_irrefl⟩
 
-instance : Std.Total fun (x1 x2 : ℕ) => ¬x1 < x2 := by
+instance : Std.Total λ x1 x2 : ℕ => ¬x1 < x2 := by
   constructor
   simp [Nat.le_total]
 
-instance : Std.Asymm fun (x1 x2 : ℕ) => x1 < x2 := ⟨λ _ _ h => Nat.lt_asymm h⟩
+instance : Std.Asymm λ x1 x2 : ℕ => x1 < x2 := ⟨λ _ _ h => Nat.lt_asymm h⟩
 
 instance :
   Trans

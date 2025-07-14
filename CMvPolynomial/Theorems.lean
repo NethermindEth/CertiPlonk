e3c1ea221f4457a360cb826338b69aa0584c2e1b
Andrei Burdusa <andrei.burdusa@zoho.com>
2025-07-14 15:38:30 +0300

    Draft

--- CMvPolynomial/Theorems.lean
+++ CMvPolynomial/Theorems.lean
@@ -1,7 +1,7 @@
 import CMvPolynomial.CMvPolynomial
 import Mathlib.Algebra.MvPolynomial.Basic
 import Mathlib.Algebra.Equiv.TransferInstance
-
+import Mathlib.RingTheory.Nullstellensatz
 open Std
 
 namespace CPoly
@@ -21,8 +21,8 @@ theorem monomial_id₁ {m : CMvMonomial n} : toCMvMonomial (fromCMvMonomial m) =
 theorem monomial_id₂ {m : Fin n →₀ ℕ} : fromCMvMonomial (toCMvMonomial m) = m := by
   ext i; aesop (add simp [toCMvMonomial, Vector.get, fromCMvMonomial]) (add safe (by omega))
 
-lemma injective_toCMvMonomial : Function.Injective (toCMvMonomial (n := n)) := 
-  Function.HasLeftInverse.injective ⟨fromCMvMonomial, fun _ ↦ monomial_id₂⟩   
+lemma injective_toCMvMonomial : Function.Injective (toCMvMonomial (n := n)) :=
+  Function.HasLeftInverse.injective ⟨fromCMvMonomial, fun _ ↦ monomial_id₂⟩
 
 def monomial_equiv : CMvMonomial n ≃ (Fin n →₀ ℕ) where
   toFun := fromCMvMonomial
@@ -30,7 +30,7 @@ def monomial_equiv : CMvMonomial n ≃ (Fin n →₀ ℕ) where
   left_inv := fun _ ↦ monomial_id₁
   right_inv := fun _ ↦ monomial_id₂
 
-def fromLawfulCMvPolynomial [CommSemiring R] (p : LawfulCMvPolynomial n R) : MvPolynomial (Fin n) R :=  
+def fromLawfulCMvPolynomial [CommSemiring R] (p : LawfulCMvPolynomial n R) : MvPolynomial (Fin n) R :=
   let support : List (Fin n →₀ ℕ) := p.monomials.map fromCMvMonomial
   let toFun (f : Fin n →₀ ℕ) : R := p[toCMvMonomial f]?.getD 0
   let mem_support_fun {a : Fin n →₀ ℕ} : a ∈ support ↔ toFun a ≠ 0 := by
@@ -51,7 +51,7 @@ def fromLawfulCMvPolynomial [CommSemiring R] (p : LawfulCMvPolynomial n R) : MvP
   Finsupp.mk support.toFinset toFun (by simp [mem_support_fun])
 
 noncomputable def toLawfulCMvPolynomial [CommSemiring R]
-  (p : MvPolynomial (Fin n) R) : LawfulCMvPolynomial n R := 
+  (p : MvPolynomial (Fin n) R) : LawfulCMvPolynomial n R :=
   let ⟨s, f, _⟩ := p
   let unlawful := Std.ExtTreeMap.ofList <| s.toList.map fun m ↦ (toCMvMonomial m, f m)
   ⟨
@@ -90,7 +90,7 @@ theorem polynomial_id₁ [CommSemiring R] {p : CMvPolynomial n R} :
   case mem => simp; grind
   case distinct =>
     simp only [Std.compare_eq_iff_eq, List.pairwise_map]
-    exact distinct_of_inj_nodup injective_toCMvMonomial (Finset.nodup_toList _)  
+    exact distinct_of_inj_nodup injective_toCMvMonomial (Finset.nodup_toList _)
 
 theorem polynomial_id₂ [CommSemiring R] {p : MvPolynomial (Fin n) R} :
   fromCMvPolynomial (toCMvPolynomial p) = p := by
@@ -112,9 +112,7 @@ theorem polynomial_id₂ [CommSemiring R] {p : MvPolynomial (Fin n) R} :
       simp only [Std.compare_eq_iff_eq, List.pairwise_map]
       exact distinct_of_inj_nodup injective_toCMvMonomial (Finset.nodup_toList _)
   · have : ∀ x ∈ s, toCMvMonomial x ≠ toCMvMonomial m := by aesop
-    grind  
-
--- TODO: use RingEquiv
+    grind
 
 noncomputable def polyEquiv [CommSemiring R] :
   Equiv (CMvPolynomial n R) (MvPolynomial (Fin n) R)
@@ -128,27 +126,59 @@ variable [CommSemiring R]
 
 noncomputable instance : CommSemiring (CMvPolynomial n R) := Equiv.commSemiring polyEquiv
 
-noncomputable def homomorphism₁ :
-  RingHom (CMvPolynomial n R) (MvPolynomial (Fin n) R)
+noncomputable def polyRingEquiv :
+  RingEquiv (CMvPolynomial n R) (MvPolynomial (Fin n) R)
 where
   toEquiv := polyEquiv
-  map_mul' := sorry
-  map_add' := sorry
+  map_mul' := by intros; unfold_projs; simp
+  map_add' := by intros; unfold_projs; simp
 
--- noncomputable def homomorphism₁ [BEq R] [CommSemiring R] [LawfulBEq R] :
---   RingHom (CMvPolynomial n R) (MvPolynomial (Fin n) R)
--- where
---   toFun := fromCMvPolynomial
---   map_one' := sorry
---   map_mul' := sorry
---   map_zero' := sorry
---   map_add' := sorry
+def fromCMvPolynomialSet (s : Set (CMvPolynomial n R)) :
+  Set (MvPolynomial (Fin n) R)
+:=
+  s ∘ toCMvPolynomial
 
--- noncomputable def homomorphism₂ [BEq R] [CommSemiring R] [LawfulBEq R] :
---   RingHom (MvPolynomial (Fin n) R) (CMvPolynomial n R)
+variable [Field K]
+
+-- variable (s : Set (CMvPolynomial n R))
+-- variable (s' : Set (MvPolynomial (Fin n) K))
+
+-- #check MvPolynomial.zeroLocus_span (λ p : MvPolynomial (Fin n) R ↦ s (toCMvPolynomial p))
+
+theorem CMvPolynomial.zeroLocus_span (S : Set (CMvPolynomial n K)) :
+  MvPolynomial.zeroLocus (Ideal.span (S ∘ toCMvPolynomial)) =
+    { x | ∀ p ∈ fromCMvPolynomialSet S, MvPolynomial.eval x p = 0 }
+:= by
+  sorry
+
+-- variable [NonAssocSemiring S₁] (f : R →+* S₁) (g : σ → S₁)
+
+-- def prod
+
+-- def sum (p : CMvPolynomial n R) (fm : CMvMonomial n → S₁) : S₁ :=
+--   let terms := p.1.toList.map λ (m, c) ↦ fm m + f c
+--   terms.sum
+
+-- /-- Evaluate a polynomial `p` given a valuation `g` of all the variables
+--   and a ring hom `f` from the scalar ring to the target -/
+-- def eval₂ (p : CMvPolynomial n R) : S₁ :=
+--   CPoly.sum f p fun s a => f a * s.prod fun n e => g n ^ e
+
+-- def eval₂Hom [NonAssocSemiring S₁](f : R →+* S₁) (g : σ → S₁) :
+--   MvPolynomial σ R →+* S₁
 -- where
---   toFun := toCMvPolynomial
---   map_one' := sorry
---   map_mul' := sorry
---   map_zero' := sorry
---   map_add' := sorry
+--   toFun := eval₂ f g
+--   map_one' := eval₂_one _ _
+--   map_mul' _ _ := eval₂_mul _ _
+--   map_zero' := eval₂_zero f g
+--   map_add' _ _ := eval₂_add _ _
+
+-- /-- Evaluate a polynomial `p` given a valuation `f` of all the variables -/
+-- def eval (f : (Fin n) → R) : CMvPolynomial n R →+* R :=
+--   eval₂Hom (RingHom.id _) f
+
+-- def zeroLocus (I : Ideal (CMvPolynomial n R)) : Set (Fin n → k) :=
+--   {x : Fin n → k | ∀ p ∈ I, eval x p = 0}
+
+-- theorem zeroLocus_span (S : Set (CMvPolynomial n R)) :
+--   zeroLocus (Ideal.span S) = { x | ∀ p ∈ S, eval x p = 0 } :=

10e1fa9951c3577b9831b10356c61753889b36fa
Daniel Britten <Coda-Coda@users.noreply.github.com>
2025-11-12 19:38:42 +1300

    Prove soundness, completeness and determinism for `IsZeroAir`

--- CertiPlonk/IsZeroAir/Constraints.lean
+++ CertiPlonk/IsZeroAir/Constraints.lean
@@ -184,6 +184,115 @@ namespace IsZeroAir.constraints
   section properties
 
     variable[Field ExtF]
+    
+    @[simp]
+    lemma is_bool {a : FBB} : a * a - a = 0 ↔ a = 0 ∨ a = 1 := by grind
+
+    theorem spec_soundness_FBB
+      {air : Valid_IsZeroAir FBB ExtF}
+      {row}
+      (r_le : row ≤ air.last_row)
+    :
+      allHold_simplified air row r_le →
+        if air.x row 0 = 0
+        then air.z row 0 = 1
+        else air.z row 0 = 0
+    := by
+      intro constraints
+      simp [IsZeroAir_constraint_and_interaction_simplification] at constraints
+      grind
+
+    theorem spec_soundness_ℕ
+      {air : Valid_IsZeroAir FBB ExtF}
+      {row}
+      (r_le : row ≤ air.last_row)
+    :
+      allHold_simplified air row r_le → 
+        if (air.x row 0).val = 0
+        then ((air.z row 0).val = 1)
+        else (air.z row 0).val = 0
+    := by
+      intro constraints
+      have := spec_soundness_FBB r_le constraints
+      grind
+      
+    theorem determinism
+      {air₁ : Valid_IsZeroAir FBB ExtF}
+      {air₂ : Valid_IsZeroAir FBB ExtF}
+      {row₁ row₂}
+      (r_le₁ : row₁ ≤ air₁.last_row)
+      (r_le₂ : row₂ ≤ air₂.last_row)
+      (h_cstrs₁ : allHold_simplified air₁ row₁ r_le₁)
+      (h_cstrs₂ : allHold_simplified air₂ row₂ r_le₂)
+      (h_eq_a : air₁.x row₁ 0 = air₂.x row₂ 0)
+    :
+      air₁.z row₁ 0 = air₂.z row₂ 0
+    := by
+      have h_eq_z : air₁.z row₁ 0 = air₂.z row₂ 0 := by
+        have := spec_soundness_FBB r_le₁ h_cstrs₁
+        have := spec_soundness_FBB r_le₂ h_cstrs₂
+        grind
+      simp [IsZeroAir_constraint_and_interaction_simplification] at h_cstrs₁ h_cstrs₂
+      obtain ⟨ h_bus₁, h0₁, h1₁, h2₁ ⟩ := h_cstrs₁
+      obtain ⟨ h_bus₂, h0₂, h1₂, h2₂ ⟩ := h_cstrs₂
+      
+      simp_all
+
+    theorem spec_completeness
+      {x : FBB}
+    :
+      ∃ (air : Valid_IsZeroAir FBB ExtF) (row : ℕ) (r_le : row ≤ air.last_row),
+        air.x row 0 = x ∧
+        allHold_simplified air row r_le
+    := by
+      let y : FBB := x⁻¹
+      let z : FBB :=
+        if x = 0
+        then 1
+        else 0
+      let isZeroAir := 
+        Raw_IsZeroAir.mk
+          (F := FBB)
+          (ExtF := ExtF)
+          (bus := List.flatMap (fun _ ↦ []) (List.range 1))
+          (challenge := fun _ ↦ 0)
+          (permutation := fun _ ↦ 0)
+          (preprocessed := fun _ ↦ 0)
+          (public_values := fun _ ↦ 0)
+          (last_row := 0)
+          (x := fun _ _ ↦ x)
+          (y := fun _ _ ↦ y)
+          (z := fun _ _ ↦ z)
+          (main := fun col row rotation =>
+            match col with
+            | 0 => x
+            | 1 => y
+            | 2 => z
+            | _ => 0 )
+      have isZeroAir_valid : isZeroAir.isValid := by
+        unfold Raw_IsZeroAir.isValid
+        unfold Raw_IsZeroAir.col_0 Raw_IsZeroAir.col_1 Raw_IsZeroAir.col_2
+        subst isZeroAir; simp_all
+      have : isZeroAir.last_row = 0 := by simp [isZeroAir]
+      exists ⟨ isZeroAir, isZeroAir_valid ⟩, isZeroAir.last_row, by simp
+      simp_all [IsZeroAir_constraint_and_interaction_simplification]
+      simp_all [Valid_IsZeroAir.bus,
+                Valid_IsZeroAir.x,
+                Valid_IsZeroAir.y,
+                Valid_IsZeroAir.z,
+                isZeroAir]
+      split_ands
+      · unfold z
+        simp
+        tauto
+      · unfold z
+        unfold y
+        by_cases h : x = 0
+        · simp [h]
+        · simp [h]
+      · unfold z
+        simp
+        tauto
 
   end properties
 
